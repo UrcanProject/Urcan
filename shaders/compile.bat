a899801c7629a3969d170c990b97f98e95a7e690
Guillaume Julien <guillaume.julien68@gmail.com>
2017-04-10 22:42:28 +0200

    Update Vulkan SDK lib from 1.0.42.1 to 1.0.46.0

--- shaders/compile.bat
+++ shaders/compile.bat
@@ -1,3 +1,3 @@
-%~dp0../lib/1.0.42.1/Bin32/glslangValidator.exe -V shader_base.vert
-%~dp0../lib/1.0.42.1/Bin32/glslangValidator.exe -V shader_base.frag
+%~dp0../lib/1.0.46.0/Bin32/glslangValidator.exe -V shader_base.vert
+%~dp0../lib/1.0.46.0/Bin32/glslangValidator.exe -V shader_base.frag
 pause
