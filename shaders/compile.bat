04bff732fdf8401547a46d45a3a2c40bb532e2ad
Guillaume Julien <guillaume.julien@epitech.eu>
2017-04-10 18:18:47 +0200

    Update compile.bat to compile with embedded Vulkan version

--- shaders/compile.bat
+++ shaders/compile.bat
@@ -1,3 +1,3 @@
-C:/VulkanSDK/1.0.42.2/Bin32/glslangValidator.exe -V shader_base.vert
-C:/VulkanSDK/1.0.42.2/Bin32/glslangValidator.exe -V shader_base.frag
+%~dp0../lib/1.0.42.1/Bin32/glslangValidator.exe -V shader_base.vert
+%~dp0../lib/1.0.42.1/Bin32/glslangValidator.exe -V shader_base.frag
 pause
