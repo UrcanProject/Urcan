9d51d04f556578fc98da1094691924bc438be8a7
Guillaume Julien <guillaume.julien68@gmail.com>
2017-04-14 12:31:33 +0200

    A few factorization to allow faster configuration

--- main.cpp
+++ main.cpp
@@ -11,38 +11,7 @@
 #include <algorithm>
 #include <fstream>
 #include "VDeleter.hpp"
-
-const int WIDTH = 800;
-const int HEIGHT = 600;
-
-const std::vector<const char *> validationLayers = {
-        "VK_LAYER_LUNARG_standard_validation"
-};
-
-const std::vector<const char *> deviceExtensions = {
-        VK_KHR_SWAPCHAIN_EXTENSION_NAME
-};
-
-struct QueueFamilyIndices {
-    int graphicsFamily = -1;
-    int presentFamily = -1;
-
-    bool isComplete() {
-        return graphicsFamily >= 0 && presentFamily >= 0;
-    }
-};
-
-struct SwapChainSupportDetails {
-    VkSurfaceCapabilitiesKHR capabilities;
-    std::vector<VkSurfaceFormatKHR> formats;
-    std::vector<VkPresentModeKHR> presentModes;
-};
-
-#ifdef NDEBUG
-const bool enableValidationLayers = false;
-#else
-const bool enableValidationLayers = true;
-#endif
+#include "BasicConfiguration.hh"
 
 VkResult CreateDebugReportCallbackEXT(VkInstance instance, const VkDebugReportCallbackCreateInfoEXT *pCreateInfo,
                                       const VkAllocationCallbacks *pAllocator, VkDebugReportCallbackEXT *pCallback) {
